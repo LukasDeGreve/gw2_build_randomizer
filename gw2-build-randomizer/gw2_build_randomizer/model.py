754038b2a61beca0228783514aea05503f6dc0fd
dangercrow <dangercrow@gmail.com>
2025-05-21 11:58:25 +0000

    Add Build class with string rendering

--- gw2-build-randomizer/gw2_build_randomizer/model.py
+++ gw2-build-randomizer/gw2_build_randomizer/model.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from enum import StrEnum, auto
-from typing import Optional, Annotated, NewType, Any
+from typing import Optional, Annotated, NewType, Any, Literal
 import random
 from pathlib import Path
 from pydantic import BaseModel, BeforeValidator
 import tomllib
+from textwrap import dedent
 
 RESOURCE_DIR = Path(__file__) / "resources"
 
@@ -52,11 +53,13 @@ class Weapon(StrEnum):
     HARPOON_GUN = auto()
     TRIDENT = auto()
 
+Skill = NewType("Skill", str)
+
 class Skills(BaseModel):
-    heal: list[str]
-    utility: list[str]
-    elite: list[str]
-    special: list[str] = []
+    heal: list[Skill]
+    utility: list[Skill]
+    elite: list[Skill]
+    special: list[Skill] = []
 
 class Profession(BaseModel):
     name: ProfessionName
@@ -66,4 +69,51 @@ class Profession(BaseModel):
 
 
 class Professions(BaseModel):
-    professions: list[Profession]
+    professions: tuple[Profession, ...]
+
+
+TraitChoice = Literal["bottom", "middle", "top"]
+
+class Trait(BaseModel):
+    specialization: Specialization
+    trait_choices: tuple[TraitChoice, TraitChoice, TraitChoice]
+
+
+class Build(BaseModel):
+    profession: Profession
+    traits: tuple[Trait, Trait, Trait]  # TODO sort by index for east
+    heal: Skill
+    utility: tuple[Skill, Skill, Skill]
+    elite: Skill
+    weapon_sets: tuple[tuple[Weapon, ...], ...]
+
+    def _render_profession_name(self) -> str:
+        maybe_elite_trait = self.traits[2]
+        spec_index = self.profession.specializations.index(maybe_elite_trait.specialization)
+        if spec_index < 5:
+            return f"Core {self.profession.name.capitalize()}"  
+        else:
+            return f"{maybe_elite_trait.specialization} ({self.profession.name.capitalize()})"
+
+    def render_for_display(self) -> str:
+        
+        return dedent(f"""
+        Your class is: {self._render_profession_name()} 
+
+        {self.traits[0].specialization}: {" ".join(self.traits[0].trait_choices)}
+
+        {self.traits[1].specialization}: {" ".join(self.traits[1].trait_choices)}
+
+        {self.traits[2].specialization}: {" ".join(self.traits[2].trait_choices)}
+
+        Heal skill: {self.heal}
+
+        Utility skill 1: {self.utility[0]}
+        Utility skill 2: {self.utility[1]}
+        Utility skill 3: {self.utility[2]}
+
+        Elite skill: {self.elite}
+
+        Weapon set 1: {", ".join(self.weapon_sets[0])}
+        {f"Weapon set 2: {", ".join(self.weapon_sets[1])}" if len(self.weapon_sets) > 1 else ""}
+        """).strip()
--- gw2-build-randomizer/tests/test_gw2_build_randomizer.py
+++ gw2-build-randomizer/tests/test_gw2_build_randomizer.py
@@ -4,7 +4,8 @@ from textwrap import dedent
 
 import pytest
 
-from gw2_build_randomizer.main import main
+from gw2_build_randomizer.main import main, get_professions
+from gw2_build_randomizer.model import Build, Trait, Profession, Professions, ProfessionName
 
 EXPECTED = {
     0: """Your class is: Catalyst (Elementalist) 
@@ -36,3 +37,30 @@ def test_can_run(capsys, seed: int) -> None:
     captured = capsys.readouterr()
     if seed in EXPECTED:
         assert captured.out == EXPECTED[seed]
+
+
+@pytest.fixture
+def professions_by_name() -> dict[ProfessionName, Professions]:
+    return {p.name: p for p in get_professions().professions}
+
+
+def test_build_rendering(professions_by_name: dict[ProfessionName, Professions]) -> None:
+    build = Build(
+        profession=professions_by_name["elementalist"],
+        traits=(
+            Trait(specialization="Fire", trait_choices=("bottom", "middle", "middle")),
+            Trait(specialization="Earth", trait_choices=("middle", "middle", "middle")),
+            Trait(specialization="Catalyst", trait_choices=("bottom", "top", "bottom")),
+        ),
+        heal="Ether Renewal",
+        utility=(
+            "Conjure Lightning Hammer",
+            "Conjure Frost Bow",
+            "Invigorating Air",
+        ),
+        elite="Tornado",
+        weapon_sets=(
+            ("scepter", "warhorn"),
+        )
+    )
+    assert build.render_for_display() == EXPECTED[0].strip()
